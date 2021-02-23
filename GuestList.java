2551c44721261f2e0c0b5698e7d8c36080e6dbe5
Butnaru Alexandru <63098264+alecsB@users.noreply.github.com>
2021-02-24 00:52:19 +0200

    Update GuestList.java

--- GuestList.java
+++ GuestList.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class GuestList {
 
-    private int numberOfSpots;
+    private int numberOfSpots;  
     private List<Guest> guests = new ArrayList<>();
     private List<Guest> waitlist = new ArrayList<>();
 
@@ -12,14 +12,14 @@ public class GuestList {
         this.numberOfSpots = numberOfSpots;
     }
 
-    public int addGuest(Guest newGuest) {
+    public int addGuest(Guest newGuest) {   // adds new guest
 
-		if (!checkGuestExistence(newGuest)) {
-			if (guests.size() < numberOfSpots) {
-				guests.add(newGuest);
+		if (!checkGuestExistence(newGuest)) {     // verifies if the guest is already signed up
+			if (guests.size() < numberOfSpots) { // verifies if number of guests is smaller then number of spots available 
+				guests.add(newGuest);	// if YES, adds guest to list and prints message
 				System.out.println("Felicitari! Locul tau la eveniment este confirmat. Te asteptam!");
 				return 0;
-			} else {
+			} else {		// if NO, adds guest to wailist
 				waitlist.add(newGuest);
 				System.out.println("Te-ai inscris cu succes in lista de asteptare si ai primit numarul de ordine " + waitlist.size() + ". Te vom notifica daca un loc devine disponibil.");
 				return waitlist.size();
@@ -29,21 +29,21 @@ public class GuestList {
 		return -1;
     }
 
-    public boolean checkGuestExistence(Guest newGuest) {
+    public boolean checkGuestExistence(Guest newGuest) {  // checks if a guest is already on list 
 
 		if (guests.contains(newGuest) || waitlist.contains(newGuest)) return true;
 
 		return false;
 	}
     
-    public Guest getGuestByFirstAndLastName(String firstName, String lastName) {
+    public Guest getGuestByFirstAndLastName(String firstName, String lastName) {   // returns the guest by name and last name criteria
     		
-    	for (Guest guest : guests) {
+    	for (Guest guest : guests) {  // iteration through guest list
     		if (guest.getFirstName().equals(firstName) && guest.getLastName().equals(lastName)) {
     			return guest;
     		}
     	}
-    	for (Guest guest : waitlist) {
+    	for (Guest guest : waitlist) {   // iteration through waitlist
     		if (guest.getFirstName().equals(firstName) && guest.getLastName().equals(lastName)) {
     			return guest;
     		}
@@ -51,14 +51,14 @@ public class GuestList {
     	return null;
     }
     
-    public Guest getGuestByEmail(String email) {
+    public Guest getGuestByEmail(String email) {    // returns guest by email criteria
 
-    	for (Guest guest : guests) {
+    	for (Guest guest : guests) {  // iteration through guest list
     		if (guest.getEmail().equals(email)) {
     			return guest;
     		}
     	}
-    	for (Guest guest : waitlist) {
+    	for (Guest guest : waitlist) {   // iteration through waitlist
     		if (guest.getEmail().equals(email)) {
     			return guest;
     		}
@@ -66,14 +66,14 @@ public class GuestList {
     	return null;
     }
     
-    public Guest getGuestByPhoneNumber(String phoneNumber) {
+    public Guest getGuestByPhoneNumber(String phoneNumber) {   // reuturns guest by phone number criteria
     	
-    	for (Guest guest : guests) {
+    	for (Guest guest : guests) {   // iteration through guest list
     		if (guest.getPhoneNumber().equals(phoneNumber)) {
     			return guest;
     		}
     	}
-    	for (Guest guest : waitlist) {
+    	for (Guest guest : waitlist) {   // iteration through waitlist
     		if (guest.getPhoneNumber().equals(phoneNumber)) {
     			return guest;
     		}
@@ -81,11 +81,11 @@ public class GuestList {
     	return null;
     }
 
-	public boolean removeGuest(Guest guest) {
+	public boolean removeGuest(Guest guest) {    // removes guest from guestlist
 
-		if (guests.contains(guest)) {
+		if (guests.contains(guest)) {    // condition for removing guest
 			guests.remove(guest);
-			if (!waitlist.isEmpty()) {
+			if (!waitlist.isEmpty()) {  // condition for adding a guest from waitlist to the available spot
 				Guest firstGuestInWaiting = waitlist.get(0);
 				guests.add(firstGuestInWaiting);
 				waitlist.remove(firstGuestInWaiting);
@@ -93,7 +93,7 @@ public class GuestList {
 			return true;
 		}
 
-		if (waitlist.contains(guest)) {
+		if (waitlist.contains(guest)) {  // condition for guest to be removed from waitlist
 			waitlist.remove(guest);
 			return true;
 		}
@@ -101,9 +101,9 @@ public class GuestList {
 		return false;
     }
 
-    public void updateGuest(Guest oldGuest, Guest newGuest) {
+    public void updateGuest(Guest oldGuest, Guest newGuest) { 
 
-		if (guests.contains(oldGuest)) {
+		if (guests.contains(oldGuest)) { 
 			guests.remove(oldGuest);
 			guests.add(newGuest);
 		} else if (waitlist.contains(oldGuest)) {
@@ -112,15 +112,15 @@ public class GuestList {
 		}
 	}
 
-	public List<Guest> getGuests() {
+    public List<Guest> getGuests() {  // returns list of guests attending to event
         return guests;
     }
 
-    public List<Guest> getWaitlistGuests() {
+    public List<Guest> getWaitlistGuests() {  // returns list of guests from waitlist
         return waitlist;
     }
 
-    public int getAvailableSpots() {
+    public int getAvailableSpots() {  // returns number of available spots
 
     	if (guests.size() == numberOfSpots) {
     		return 0;
@@ -129,21 +129,21 @@ public class GuestList {
     	return numberOfSpots - guests.size();
     }
 
-    public int getNumberOfGuests() {
+    public int getNumberOfGuests() {    // returns number of people that will attend the event
         return guests.size();
     }
 
-    public int getNumberOfGuestsOnWaitlist() {
+    public int getNumberOfGuestsOnWaitlist() { //   returns number of guests from waiting list
         return waitlist.size();
     }
 
-    public int getAllGuests() {
+    public int getAllGuests() {     // returns number of total guests at event(on guest list + waitlist);
         return guests.size() + waitlist.size();
     }
 
-    public List<Guest> partialSearch(String searchTerm) {
+    public List<Guest> partialSearch(String searchTerm) {   // searches for guest according to the substring given by user
 
-    	List<Guest> matchingGuests = new ArrayList<>();
+    	List<Guest> matchingGuests = new ArrayList<>();   
 
 		Guest matchingGuest = getGuestWithMatchingFieldFromList(searchTerm, guests);
 		if (matchingGuest != null) {
@@ -158,7 +158,7 @@ public class GuestList {
 		return matchingGuests;
     }
 
-	private Guest getGuestWithMatchingFieldFromList(String searchTerm, List<Guest> list) {
+	private Guest getGuestWithMatchingFieldFromList(String searchTerm, List<Guest> list) {   // returns the guest after partial search by substring
 
 		for (Guest guest : list) {
 			if (guestFieldContainsTerm(guest.getFirstName(), searchTerm)
